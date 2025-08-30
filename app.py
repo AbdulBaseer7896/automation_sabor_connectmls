adef256f78f75bba7c2097e683dd604bd0e3df09
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2025-08-30 11:03:49 +0500

    now add the apt.txt again

--- app.py
+++ app.py
@@ -19,11 +19,14 @@ app = Flask(__name__)
 def run_selenium():
 
     chrome_options = Options()
+    chrome_options.add_argument('--headless=new')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()), options=chrome_options)
+    # driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()), options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         # driver = webdriver.Chrome(service=service, options=chrome_options)
